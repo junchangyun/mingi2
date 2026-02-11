a9ee0b9b999a37a2ebc437cd5b18fc91436f6518
jcystar19616056 <jcystar19616056@gmail.com>
2026-02-11 02:46:00 +0000

    update project

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-// Add JS here
+const numbersContainer = document.querySelector('.numbers');
+const generateBtn = document.querySelector('#generate');
+
+const generateNumbers = () => {
+    const numbers = new Set();
+    while (numbers.size < 6) {
+        numbers.add(Math.floor(Math.random() * 45) + 1);
+    }
+    return [...numbers];
+};
+
+const displayNumbers = (numbers) => {
+    numbersContainer.innerHTML = '';
+    numbers.forEach(number => {
+        const numberEl = document.createElement('div');
+        numberEl.classList.add('number');
+        numberEl.textContent = number;
+        numbersContainer.appendChild(numberEl);
+    });
+};
+
+generateBtn.addEventListener('click', () => {
+    const numbers = generateNumbers();
+    displayNumbers(numbers);
+});
